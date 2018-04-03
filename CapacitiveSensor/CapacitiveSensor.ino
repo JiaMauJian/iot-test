71f81938038e68f62b58c83a694da2683779dee8
JiaMauJian <amau712@gmail.com>
2018-04-03 13:57:16 +0800

    Add files via upload

---
+++ CapacitiveSensor/CapacitiveSensor.ino
@@ -0,0 +1,26 @@
+#include <CapacitiveSensor.h>
+
+CapacitiveSensor cs_4_5 = CapacitiveSensor(4, 5);
+
+void setup() {    
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  long start = millis();
+  long total = cs_4_5.capacitiveSensor(30);
+
+  Serial.print(millis() - start);
+  Serial.print("\t");
+
+  Serial.println(total);
+
+  if (total > 50)
+    digitalWrite(13, HIGH);
+
+  if (total == 0)
+    digitalWrite(13, LOW);
+    
+  delay(10);
+}
